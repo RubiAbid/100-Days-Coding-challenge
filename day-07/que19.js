f90af881b0181a7dd35f2d7e5af07a54d9d44d28
RubiAbid <rubiridz@gmail.com>
2024-06-12 14:31:26 +0100

    Added Challenge Questions for day 7

---
+++ day-07/que19.js
@@ -0,0 +1,6 @@
+/*       Question 19:
+ Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message
+  indicating the number of people you are inviting to dinner.*/
+var invitation = ['maham', 'waniya', 'sidra'];
+var lengths = invitation.length;
+console.log("I am inviting total ".concat(lengths, " guests"));
---
+++ day-07/que19.ts
@@ -0,0 +1,8 @@
+/*       Question 19:
+ Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message
+  indicating the number of people you are inviting to dinner.*/
+
+  
+let invitation:string[]=['maham','waniya','sidra']
+let lengths=invitation.length;
+console.log(`I am inviting total ${lengths} guests`)
