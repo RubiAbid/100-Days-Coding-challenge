e3b7e0262de9b5e45df4097a71568998ef9932b9
RubiAbid <rubiridz@gmail.com>
2024-06-14 19:35:13 +0100

    Added Challenge Questions for day 10

---
+++ day-10/que29.js
@@ -0,0 +1,21 @@
+/*Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that
+check for certain fruits in your array.
+• Make a array of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of fruit is in your array.
+If the fruit is in your array, the if block should print a statement, such as You really like bananas!*/
+var favfruits = ['mango', 'lichi', 'cherry'];
+if (favfruits.includes('mango')) {
+    console.log("You really like mangoes");
+}
+if (favfruits.includes('cherry')) {
+    console.log("You really like cherry");
+}
+if (favfruits.includes('lichi')) {
+    console.log("You really like lichi");
+}
+if (favfruits.includes('banana')) {
+    console.log("You really like bananas");
+}
+if (favfruits.includes('berry')) {
+    console.log("You really like berry");
+}
