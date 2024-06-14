580ebfb206f3ff214c1ebf7d9d3bd3662f5e59e5
RubiAbid <rubiridz@gmail.com>
2024-06-14 19:31:32 +0100

    Added Challenge Question for day 9

---
+++ day-09/que25.ts
@@ -0,0 +1,17 @@
+/*               Question 25:
+ Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 
+ 'green', 'yellow',or 'red'.
+• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)*/
+
+
+//version1
+let alien_color='green';
+if(alien_color==='green'){
+    console.log("the player just earned 5 points")
+}
+  //version2
+ let aliencolor = "blue";
+if (alien_color == "purple") {
+    // No output because the condition is false
+}
