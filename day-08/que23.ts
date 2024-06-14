f9a6a70dfc53017bfa2d16706f96821385ed6594
RubiAbid <rubiridz@gmail.com>
2024-06-14 07:20:50 +0100

    Added Challenge Questions for Day 8

---
+++ day-08/que23.ts
@@ -0,0 +1,30 @@
+/*         Question 23: 
+Conditional Tests: Write a series of conditional tests. Predict the results of each test.*/
+
+let city = 'Karachi';
+let fruit = 'apple';
+let favoriteBook = 'mushaf';
+let movie = 'Transformer';
+let score = 95;
+// True tests
+console.log("Is city =='Karachi'? I predict True.");
+console.log(city == 'Karachi');
+console.log("Is fruit == 'apple'? I predict True.");
+console.log(fruit == 'apple');
+console.log("Is favoriteBook == 'mushaf'? I predict True.");
+console.log(favoriteBook == 'mushaf');
+console.log("Is movie == 'Transformer'? I predict True.");
+console.log(movie == 'Transformer');
+console.log("Is score == 95? I predict True.");
+console.log(score == 95)
+//False tests
+console.log("Is city == 'Lahore'? I predict False.");
+console.log(city == 'Lahore');
+console.log("Is fruit == 'banana'? I predict False.");
+console.log(fruit == 'banana');
+console.log("Is favoriteBook == '1984'? I predict False.");
+console.log(favoriteBook == '1984');
+console.log("Is movie == 'Titanic'? I predict False.");
+console.log(movie == 'Titanic');
+console.log("Is score == 100? I predict False.");
+console.log(score == 100);
