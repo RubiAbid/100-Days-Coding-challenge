d4f115fb98df379e2b53fe283690c870c2beb519
RubiAbid <rubiridz@gmail.com>
2024-07-04 16:03:14 +0500

    Added Challenge questions for day 25

---
+++ day-25/que73.js
@@ -0,0 +1,10 @@
+/*          Question 73:
+ Assigning and Updating Variables: Create a function where you declare a variable using let and assign an
+  initial value. Then, update its value within the same function and log both the initial and updated values.*/
+function updateVariable() {
+    var num = 10; // Initial value
+    console.log('Initial value:', num);
+    num = 20; // Updated value
+    console.log('Updated value:', num);
+}
+updateVariable();
---
+++ day-25/que75.js
@@ -0,0 +1,19 @@
+/*                 Question 75:
+ Compound Assignment Operators: Use compound assignment operators to simplify arithmetic operations.
+  Start with x = 4 and perform a series of operations (addition, subtraction, multiplication, division)
+  on x using compound operators.*/
+function compoundassignment(x) {
+    // Adding 6 to x
+    x += 6;
+    console.log('Addition:', x); // Output: Addition: 10
+    // Subtracting 2 from x
+    x -= 2;
+    console.log('Subtraction:', x); // Output: Subtraction: 8
+    // Multiplying x by 4
+    x *= 4;
+    console.log('Multiplication:', x); // Output: Multiplication: 32
+    // Dividing x by 4
+    x; //= 4;
+    console.log('Division:', x); // Output: Division: 8
+}
+compoundassignment(4);
