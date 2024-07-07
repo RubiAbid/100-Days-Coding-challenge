d3a592852c1fead239f982bfa4de50789a992a82
RubiAbid <rubiridz@gmail.com>
2024-07-07 21:06:46 +0500

    Added Challenge Questions for day 28

---
+++ day-28/que82.ts
@@ -0,0 +1,16 @@
+/*                Question 82: 
+Find the Length of a String: Write a function that takes a string 
+as an argument and returns the number of characters in the string.*/
+
+
+
+
+// Function to calculate the length of a given string
+function lengthofstr(word: string): number {
+    // Return the length of the input string
+    return word.length;
+}
+
+// Log the length of the string "Rubi" to the console
+console.log(lengthofstr("coding"));  
+
---
+++ day-28/que83.js
@@ -0,0 +1,12 @@
+/*              Question 83:
+Converting to Upper and Lower Case: Create a function that takes
+a string, converts it to uppercase, then to lowercase, and logs both.*/
+// Function to log the lowercase and uppercase versions of a given string
+function cases(str) {
+    // Convert the input string to lowercase and log the result
+    console.log(str.toLowerCase());
+    // Convert the input string to uppercase and log the result
+    console.log(str.toUpperCase());
+}
+// Call the function with the string "tyPeScriPt"
+cases("tyPeScriPt");
